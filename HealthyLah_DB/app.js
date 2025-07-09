a9a7993123d703c09799f4c853beaf6e13700b44
Justin Tang <jtang8446@gmail.com>
2025-07-09 13:32:22 +0800

    implement register, login feature with basic authorization and authentication

--- HealthyLah_DB/app.js
+++ HealthyLah_DB/app.js
@@ -6,32 +6,40 @@ const cors = require("cors");
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-
 // Controllers
+const userController = require("./controllers/user_controller");
 const weatherController = require("./controllers/weather_controller");
 const appointmentController = require("./controllers/appointment_controller");
 const medicationsController = require('./controllers/medications_controller');
 const { translateText } = require("./controllers/translation_controller");
 
+// Create express app
+const app = express();
+const port = process.env.PORT || 3000;
+
 // Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 const appointmentValidator = require("./middlewares/appointment_validation");
 const medicationValidator = require("./middlewares/medication_validation");
 
 // Routes
+// User Route
+app.get("/users", userController.getAllUsers);
+app.post("/users", userController.registerUser);
+app.post("/users/login", userController.loginUser);
+
+// Weather Route
 app.get("/weather", weatherController.getWeather);
 
+// Appointment route
 app.get("/appointments", appointmentController.getAllAppointments);
 app.get("/appointments/user/:userID", appointmentValidator.validateAppointmentId, appointmentController.getAppointmentsByUserID);
 app.post("/appointments/user", appointmentValidator.validateAppointment, appointmentController.createAppointment);
 
+// Medication Route
 app.get("/medications/today", medicationsController.getTodayMeds);
 app.post("/medications", medicationValidator, medicationsController.addMedication);
 app.patch("/medications/:medicationID/mark-taken", medicationsController.markTaken);
---
+++ HealthyLah_DB/models/user_model.js
@@ -0,0 +1,96 @@
+const sql = require("mssql");
+const dbConfig = require("../dbConfig");
+
+// Get all users
+async function getAllUsers(){
+    let connection;
+    try {
+        connection = await sql.connect(dbConfig);
+        const query = `SELECT * FROM Users`;
+        const result = await connection.request().query(query);
+        return result.recordset;
+    }catch(error){
+        console.error("Database error: ", error);
+        throw error;
+    }finally{
+        if (connection){
+            try{
+                await connection.close();
+            }catch(closeError){
+                console.error("Error closing connection: ", closeError);
+            }
+        }
+    }
+}
+
+// Get user by username
+async function getUserByUsername(username){
+    let connection;
+    try{
+        connection = await sql.connect(dbConfig);
+        const query = `SELECT u.userID, fullName, username, email, passwordHash, gender 
+                       FROM Users u INNER JOIN UserProfile up 
+                       ON u.userID = up.userID 
+                       WHERE username = @username`;
+        const request = connection.request();
+        request.input("username", username);
+        const result = await request.query(query);
+        
+        if (result.recordset.length === 0){
+            return null // User not found
+        }
+
+        return result.recordset[0];
+    }catch(error){
+        console.error("Database error: ",  error);
+        throw error;
+    }finally{
+        if(connection){
+            try{
+                await connection.close();
+            }catch(closeError){
+                console.error("Error closing connection: ", closeError);
+            }
+        }
+    }
+}
+
+// Create new user
+async function createUser(userData){
+    let connection;
+    try{
+        connection = await sql.connect(dbConfig);
+        const query = 
+           `INSERT INTO Users (fullName, username, email, passwordHash) VALUES (@fullName, @username, @email, @passwordHash); 
+            DECLARE @newUserID INT = SCOPE_IDENTITY();
+            INSERT INTO UserProfile(userID) VALUES (@newUserID)
+            SELECT * FROM Users WHERE userID = @newUserID;`;
+            // Declares get the new userID as a variable
+        const request = connection.request();
+        request.input("fullName", userData.fullName)
+        request.input("username", userData.username);
+        request.input("email", userData.email);
+        request.input("passwordHash", userData.passwordHash);
+        result = await request.query(query);
+
+        const newUsername = result.recordset[0].username;
+        return await getUserByUsername(newUsername);
+    }catch(error){
+        console.error("Database error: ", error);
+        throw error;
+    }finally{
+        if (connection){
+            try{
+                await connection.close();
+            }catch(closeError){
+                console.error("Error closing connection: ", closeError);
+            }
+        }
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getUserByUsername,
+    createUser,
+}
